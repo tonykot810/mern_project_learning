f0b37ec482d4cace0f04fadf8041475a2ed3bd27
tonykot810 <tonykot10@gmail.com>
2022-05-26 21:16:19 +0800

    update post form

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import {Container , AppBar, Typography, Grow ,Grid } from '@material-ui/core';
 
+import Posts from'./components/Posts/Posts'
+import Form from'./components/Form/Form'
 import memories from './images/memories.png'
 function App() {
   return (
