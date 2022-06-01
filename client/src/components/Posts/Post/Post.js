32eb338d172b8315f212698c6d80f02063fb017e
tonykot810 <tonykot10@gmail.com>
2022-06-01 21:39:48 +0800

    edit post function

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -6,7 +6,7 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import moment from 'moment'
 import useStyles from './styles';
 
-const Post = ({post}) =>{
+const Post = ({post, setCurrentId}) =>{
     const classes = useStyles();
     return(
     <Card className={classes.card}>
@@ -16,7 +16,7 @@ const Post = ({post}) =>{
             <Typography variant="body2">{moment(post.cratedAt).fromNow()}</Typography>
         </div>
         <div className={classes.overlay2}>
-            <Button style ={{color: 'white'}} size="small" onClick={() => {}}><MoreHorizIcon fontSize="medium" /></Button>
+            <Button style ={{color: 'white'}} size="small" onClick={() => {setCurrentId(post._id)}}><MoreHorizIcon fontSize="medium" /></Button>
         </div>
         <div className={classes.details}>
             <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
