34d5494801318631a6cdd1ca33da46e67cac048c
tonykot810 <tonykot10@gmail.com>
2022-06-02 22:12:08 +0800

    Delete Post function

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -5,9 +5,13 @@ import DeleteIcon from '@material-ui/icons/Delete'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import moment from 'moment'
 import useStyles from './styles';
+import { useDispatch } from "react-redux";
+import { deletePost  } from "../../../actions/posts";
+
 
 const Post = ({post, setCurrentId}) =>{
     const classes = useStyles();
+    const dispatch = useDispatch();
     return(
     <Card className={classes.card}>
         <CardMedia className={classes.media} image={post.selectedFile|| 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title ={post.title}/>
@@ -31,7 +35,7 @@ const Post = ({post, setCurrentId}) =>{
             Like
             {post.likeCount}
             </Button>
-            <Button size="small" color="primary" onClick={() => {}}>
+            <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
             <DeleteIcon fontSize="small"/>
             Delete
             </Button>
