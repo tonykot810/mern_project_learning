7281948e8b7a16ba6523ca7960fae2af7f30114b
tonykot810 <tonykot10@gmail.com>
2022-06-20 18:32:32 +0800

    update

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ function App() {
   return (
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position='static' color="inherit">
-        <Typography display="inline" className={classes.heading}variant='h2' align='center'>Memeories
+        <Typography display="inline" className={classes.heading}variant='h2' align='center'>Blog
         <img className={classes.image} src={memories} alt="memories" height="60"/>
         </Typography>
       </AppBar>
