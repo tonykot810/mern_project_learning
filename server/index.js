2fbe50469f038d5d760a176c4b50e41bcde9b598
tonykot810 <tonykot10@gmail.com>
2022-06-22 21:41:57 +0800

    add back port

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) =>{
     res.send('Connect blog API')
 })
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology:true})
 .then (()=> app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
